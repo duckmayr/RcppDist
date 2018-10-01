3b24180b8e4d7c0d3679ad7e26fe922e49418b13
JB Duck-Mayr <j.duckmayr@gmail.com>
2018-10-01 15:20:29 -0500

    Add copyright/licence infor to headers, update vignette

--- inst/include/mvt.h
+++ inst/include/mvt.h
@@ -1,3 +1,22 @@
+// mvt.h
+//
+// Copyright (C) 2018 JB Duck-Mayr
+//
+// This file is part of RcppDist.
+//
+// RcppDist is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// RcppDist is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RcppDist.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef RCPPDIST_MVT_H
 #define RCPPDIST_MVT_H
 
